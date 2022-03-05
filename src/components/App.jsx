67be4f4a0d16b4f1c4a4655f23eea931fe39e37a
LeviRomans <40896186+LeviRomans@users.noreply.github.com>
2022-03-05 15:01:38 -0500

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Note />
+      <Footer />
+    </>
+  );
+};
+
+export default App;

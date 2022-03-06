49ee2e0ba3be57eeb1ffed19e936f290ecc0c1d6
LeviRomans <40896186+LeviRomans@users.noreply.github.com>
2022-03-05 20:50:25 -0500

    React App with Header and Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Footer = () => {
-  return <></>;
+  let currentYear = new Date().getFullYear();
+  return (
+    <div>
+      <footer>
+        <p>Copyright {currentYear} Chip's Chimney </p>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
